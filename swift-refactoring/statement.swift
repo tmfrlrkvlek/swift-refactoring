fa926096f763935ea4eecf6ae1e91fc8132cd121
JISU LEE <wltn102998@gmail.com>
2024-04-21 20:37:28 +0900

    refactor: #1.4.1 switch문에 주목해서 statement() 함수 분리

--- swift-refactoring/statement.swift
+++ swift-refactoring/statement.swift
@@ -20,30 +20,13 @@ func statement(invoice: Invoice, plays: Plays) -> String {
 
     for performance in invoice.performances {
         guard let play = plays[performance.playID] else { continue }
-        var amount = 0
+        let amount = amountFor(performance: performance, with: play)
 
-        switch play.type {
-        case .tragedy : // 비극
-            amount = 40000
-            if (performance.audience > 30) {
-                amount += 1000 * (performance.audience - 30)
-            }
-        case .comedy : // 희극
-            amount = 30000
-            if (performance.audience > 20) {
-                amount += 10000 + 500 * (performance.audience - 20)
-            }
-            amount += 300 * performance.audience
-        }
-
-        // 포인트를 적립한다.
         volumeCredits += max(performance.audience - 30, 0)
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
         if (play.type == .comedy) {
             volumeCredits += Int(performance.audience / 5)
         }
 
-        // 청구 내역을 출력한다.
         result += " \(play.name): $\(amount/100) (\(performance.audience)석)\n"
         totalAmount += amount
     }
@@ -51,5 +34,23 @@ func statement(invoice: Invoice, plays: Plays) -> String {
     result += "총액: $\(totalAmount/10)\n"
     result += "적립 포인트: \(volumeCredits)점\n"
     return result
+    
+    func amountFor(performance: Performance, with play: Play) -> Int {
+        var amount = 0
+        switch play.type {
+        case .tragedy :
+            amount = 40000
+            if (performance.audience > 30) {
+                amount += 1000 * (performance.audience - 30)
+            }
+        case .comedy :
+            amount = 30000
+            if (performance.audience > 20) {
+                amount += 10000 + 500 * (performance.audience - 20)
+            }
+            amount += 300 * performance.audience
+        }
+        return amount
+    }
 }
 

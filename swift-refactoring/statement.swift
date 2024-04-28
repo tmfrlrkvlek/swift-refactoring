163d7d87740ea05705eab406a79e66091ad28ac8
JISU LEE <wltn102998@gmail.com>
2024-04-28 17:47:55 +0900

    refactor: #1.6.5 amountFor() 함수를 statement(...) 함수로 옮기기

--- swift-refactoring/statement.swift
+++ swift-refactoring/statement.swift
@@ -22,6 +22,7 @@ private typealias StatementData = (customer: String, performances: [EnrichedPerf
 private struct EnrichedPerformance {
     let play: Play
     let audience: Int
+    let amount: Int
 }
 
 func statement(invoice: Invoice, plays: Plays) throws -> String {
@@ -35,7 +36,16 @@ func statement(invoice: Invoice, plays: Plays) throws -> String {
     return renderPlainText(data: data)
     
     func enrich(_ performance: Performance) throws -> EnrichedPerformance {
-        return EnrichedPerformance(play: try playFor(performance), audience: performance.audience)
+        let intermediateResult = EnrichedPerformance(
+            play: try playFor(performance),
+            audience: performance.audience,
+            amount: 0
+        )
+        return EnrichedPerformance(
+            play: intermediateResult.play,
+            audience: intermediateResult.audience,
+            amount: amountFor(intermediateResult)
+        )
     }
     
     func playFor(_ performance: Performance) throws -> Play {
@@ -44,12 +54,30 @@ func statement(invoice: Invoice, plays: Plays) throws -> String {
         }
         return play
     }
+    
+    func amountFor(_ performance: EnrichedPerformance) -> Int {
+        var result = 0
+        switch performance.play.type {
+        case .tragedy :
+            result = 40000
+            if (performance.audience > 30) {
+                result += 1000 * (performance.audience - 30)
+            }
+        case .comedy :
+            result = 30000
+            if (performance.audience > 20) {
+                result += 10000 + 500 * (performance.audience - 20)
+            }
+            result += 300 * performance.audience
+        }
+        return result
+    }
 }
 
 private func renderPlainText(data: StatementData) -> String {
     var result = "청구 내역 (고객명: \(data.customer))\n"
     for performance in data.performances {
-        result += " \(performance.play.name): $\(amountFor(performance)/100) (\(performance.audience)석)\n"
+        result += " \(performance.play.name): $\(performance.amount/100) (\(performance.audience)석)\n"
     }
     result += "총액: $\(totalAmount()/10)\n"
     result += "적립 포인트: \(totalVolumeCredits())점\n"
@@ -58,7 +86,7 @@ private func renderPlainText(data: StatementData) -> String {
     func totalAmount() -> Int {
         var result = 0
         for performance in data.performances {
-            result += amountFor(performance)
+            result += performance.amount
         }
         return result
     }
@@ -76,24 +104,6 @@ private func renderPlainText(data: StatementData) -> String {
         result += max(performance.audience - 30, 0)
         if (performance.play.type == .comedy) {
             result += Int(performance.audience / 5)
-        }
-        return result
-    }
-    
-    func amountFor(_ performance: EnrichedPerformance) -> Int {
-        var result = 0
-        switch performance.play.type {
-        case .tragedy :
-            result = 40000
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30)
-            }
-        case .comedy :
-            result = 30000
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20)
-            }
-            result += 300 * performance.audience
         }
         return result
     }

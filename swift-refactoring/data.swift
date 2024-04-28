f96c4ee3288cd4c62d0941fa8b98f5038a8aa6fe
JISU LEE <wltn102998@gmail.com>
2024-04-28 18:37:08 +0900

    refactor: #1.7 두 파일로 분리하고 HTML 생성하는 코드 추가

---
+++ swift-refactoring/data.swift
@@ -0,0 +1,107 @@
+//
+//  data.swift
+//  swift-refactoring
+//
+//  Created by 이지수 on 4/28/24.
+//
+
+import Foundation
+
+enum StatementError: Error {
+    case playIDNotMatched
+}
+
+struct StatementData {
+    let customer: String
+    let performances: [EnrichedPerformance]
+}
+
+struct IntermediatePerformance {
+    let play: Play
+    let audience: Int
+}
+
+struct EnrichedStatementData {
+    let customer: String
+    let performances: [EnrichedPerformance]
+    let totalAmount: Int
+    let totalVolumeCredits: Int
+}
+
+struct EnrichedPerformance {
+    let play: Play
+    let audience: Int
+    let amount: Int
+    let volumeCredits: Int
+}
+
+func createStatementData(invoice: Invoice, plays: Plays) throws -> EnrichedStatementData {
+    return enrich(StatementData(
+        customer: invoice.customer,
+        performances: try invoice.performances.map(enrich(_:)))
+    )
+    
+    func enrich(_ data: StatementData) -> EnrichedStatementData {
+        return EnrichedStatementData(
+            customer: data.customer,
+            performances: data.performances,
+            totalAmount: totalAmount(data: data),
+            totalVolumeCredits: totalVolumeCredits(data: data)
+        )
+    }
+    
+    func enrich(_ performance: Performance) throws -> EnrichedPerformance {
+        let intermediateResult = IntermediatePerformance(
+            play: try playFor(performance),
+            audience: performance.audience
+        )
+        return EnrichedPerformance(
+            play: intermediateResult.play,
+            audience: intermediateResult.audience,
+            amount: amountFor(intermediateResult),
+            volumeCredits: volumeCreditsFor(intermediateResult)
+        )
+    }
+    
+    func playFor(_ performance: Performance) throws -> Play {
+        guard let play =  plays[performance.playID] else {
+            throw StatementError.playIDNotMatched
+        }
+        return play
+    }
+    
+    func amountFor(_ performance: IntermediatePerformance) -> Int {
+        var result = 0
+        switch performance.play.type {
+        case .tragedy :
+            result = 40000
+            if (performance.audience > 30) {
+                result += 1000 * (performance.audience - 30)
+            }
+        case .comedy :
+            result = 30000
+            if (performance.audience > 20) {
+                result += 10000 + 500 * (performance.audience - 20)
+            }
+            result += 300 * performance.audience
+        }
+        return result
+    }
+    
+    func volumeCreditsFor(_ performance: IntermediatePerformance) -> Int {
+        var result = 0
+        result += max(performance.audience - 30, 0)
+        if (performance.play.type == .comedy) {
+            result += Int(performance.audience / 5)
+        }
+        return result
+    }
+    
+    func totalAmount(data: StatementData) -> Int {
+        return data.performances.map({ $0.amount }).reduce(0, +)
+    }
+    
+    func totalVolumeCredits(data: StatementData) -> Int {
+        return data.performances.map({ $0.volumeCredits }).reduce(0, +)
+    }
+}
--- swift-refactoring/statement.swift
+++ swift-refactoring/statement.swift
@@ -13,108 +13,10 @@ import Foundation
  2. 연극 장르와 공연료 정책이 달라질 때마다 statement() 함수를 수정해야 함
  **/
 
-enum StatementError: Error {
-    case playIDNotMatched
-}
-
-private struct StatementData {
-    let customer: String
-    let performances: [EnrichedPerformance]
-}
-
-private struct IntermediatePerformance {
-    let play: Play
-    let audience: Int
-}
-
-private struct EnrichedStatementData {
-    let customer: String
-    let performances: [EnrichedPerformance]
-    let totalAmount: Int
-    let totalVolumeCredits: Int
-}
-
-private struct EnrichedPerformance {
-    let play: Play
-    let audience: Int
-    let amount: Int
-    let volumeCredits: Int
-}
-
 func statement(invoice: Invoice, plays: Plays) throws -> String {
     return renderPlainText(data: try createStatementData(invoice: invoice, plays: plays))
 }
 
-private func createStatementData(invoice: Invoice, plays: Plays) throws -> EnrichedStatementData {
-    return enrich(StatementData(
-        customer: invoice.customer,
-        performances: try invoice.performances.map(enrich(_:)))
-    )
-    
-    func enrich(_ data: StatementData) -> EnrichedStatementData {
-        return EnrichedStatementData(
-            customer: data.customer,
-            performances: data.performances,
-            totalAmount: totalAmount(data: data),
-            totalVolumeCredits: totalVolumeCredits(data: data)
-        )
-    }
-    
-    func enrich(_ performance: Performance) throws -> EnrichedPerformance {
-        let intermediateResult = IntermediatePerformance(
-            play: try playFor(performance),
-            audience: performance.audience
-        )
-        return EnrichedPerformance(
-            play: intermediateResult.play,
-            audience: intermediateResult.audience,
-            amount: amountFor(intermediateResult),
-            volumeCredits: volumeCreditsFor(intermediateResult)
-        )
-    }
-    
-    func playFor(_ performance: Performance) throws -> Play {
-        guard let play =  plays[performance.playID] else {
-            throw StatementError.playIDNotMatched
-        }
-        return play
-    }
-    
-    func amountFor(_ performance: IntermediatePerformance) -> Int {
-        var result = 0
-        switch performance.play.type {
-        case .tragedy :
-            result = 40000
-            if (performance.audience > 30) {
-                result += 1000 * (performance.audience - 30)
-            }
-        case .comedy :
-            result = 30000
-            if (performance.audience > 20) {
-                result += 10000 + 500 * (performance.audience - 20)
-            }
-            result += 300 * performance.audience
-        }
-        return result
-    }
-    
-    func volumeCreditsFor(_ performance: IntermediatePerformance) -> Int {
-        var result = 0
-        result += max(performance.audience - 30, 0)
-        if (performance.play.type == .comedy) {
-            result += Int(performance.audience / 5)
-        }
-        return result
-    }
-    
-    func totalAmount(data: StatementData) -> Int {
-        return data.performances.map({ $0.amount }).reduce(0, +)
-    }
-    
-    func totalVolumeCredits(data: StatementData) -> Int {
-        return data.performances.map({ $0.volumeCredits }).reduce(0, +)
-    }
-}
 
 private func renderPlainText(data: EnrichedStatementData) -> String {
     var result = data.performances.reduce("청구 내역 (고객명: \(data.customer))\n", { before, performance in
@@ -124,4 +26,22 @@ private func renderPlainText(data: EnrichedStatementData) -> String {
     result += "총액: $\(data.totalAmount/10)\n"
     result += "적립 포인트: \(data.totalVolumeCredits)점\n"
     return result
+}
+
+func htmlStatement(invoice: Invoice, plays: Plays) throws -> String {
+    return renderHtml(data: try createStatementData(invoice: invoice, plays: plays))
+}
+
+private func renderHtml(data: EnrichedStatementData) -> String {
+    var result = """
+<h1>청구 내역 (고객명: \(data.customer))</h1>
+<table>
+<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+"""
+    result = data.performances.reduce(result, { before, performance in
+        before + """
+    <tr><td>\(performance.play.name)</td><td>\(performance.audience)</td><td>$\(performance.amount / 10)</td>
+"""
+    })
+    return result + "</table>\n"
 }
